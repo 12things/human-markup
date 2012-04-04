89adba67d3c347ee36978c6148347be41c6707fb
Hannes Schippmann <hannes@schippmann.tv>
2012-04-04 15:55:16 +0200

    Added headings, paragraphs and bolds.

--- playground/javascripts/human_markup.coffee
+++ playground/javascripts/human_markup.coffee
@@ -1,5 +1,8 @@
 class HumanMarkup
-  @heading_regex = /([\w ,\.-_!?]*)\n\n/g
+  @h1_regex = /^(\w.*[^\W])$(\n\n+)/gm
+  @h2_regex = /^(\w.*[^\W])$(\n)/gm
+  @p_regex = /^(\w.*[\W])$(\n\n+)/gm
+  @strong_regex = /([\w ]*[.?])?([\w. ]*[!])/g
   
   constructor: (@input, @html, @output) ->
     @input.on 'keyup', () =>
@@ -9,22 +12,64 @@ class HumanMarkup
     @html.text @.process(@input.val())
     @output.html @.process(@input.val())
     
-  process: (input) ->
-    result = input
-    result = @.detectHeading result
+  process: (text) ->
+    text = @.markdownBefore text
+
+    # Human Markup
+    text = @.detectHeadings text
+    text = @.detectParagraphs text
+    text = @.detectBolds text
+
+    text = @.markdownAfter text
+                
+  detectHeadings: (text) ->
+    text = text.replace HumanMarkup.h1_regex, "<h1>$1</h1>\n\n"
+    text = text.replace HumanMarkup.h2_regex, "<h2>$1</h2>\n\n"
+
+  detectParagraphs: (text) ->
+    text = text.replace HumanMarkup.p_regex, "<p>$1</p>\n\n"
+
+  detectBolds: (text) ->
+    text = text.replace HumanMarkup.strong_regex, "$1<strong>$2</strong>"
     
-    result
-  
-  detectHeading: (input) ->
-    output = input.replace HumanMarkup.heading_regex, "<h1>$1</h1>\n"
-    output
+  markdownBefore: (text) ->
+    # attacklab: Replace ~ with ~T
+    # This lets us use tilde as an escape char to avoid md5 hashes
+    # The choice of character is arbitray; anything that isn't
+    # magic in Markdown will work.
+    text = text.replace(/~/g, "~T")
+            
+    # attacklab: Replace $ with ~D
+    # RegExp interprets $ as a special character
+    # when it's in a replacement string
+    text = text.replace(/\$/g, "~D")
+
+    # Standardize line endings
+    text = text.replace(/\r\n/g, "\n") # DOS to Unix
+    text = text.replace(/\r/g, "\n")  # Mac to Unix
+
+    # Make sure text begins and ends with a couple of newlines:
+    # text = "\n\n" + text + "\n\n"
+
+    # Strip any lines consisting only of spaces and tabs.
+    # This makes subsequent regexen easier to write, because we can
+    # match consecutive blank lines with /\n+/ instead of something
+    # contorted like /[ \t]*\n+/ .
+    text = text.replace(/^[ \t]+$/mg, "");
+
+  markdownAfter: (text) ->
+    # attacklab: Restore dollar signs
+    text = text.replace(/~D/g, "$$")
+    # attacklab: Restore tildes
+    text = text.replace(/~T/g, "~");
+
 
 $ ->
-  new HumanMarkup($('#input'), $('#html'), $('#output'))
+  window.hm = new HumanMarkup($('#input'), $('#html'), $('#output'))
   
   converter = new Markdown.Converter()
   
-  filling = """
+  markdown = """
 # Hi, I'm Markdown
 
 What can **I do** for *you*?
@@ -82,11 +127,34 @@ two spaces
 
 [de]: http://google.de/  "German Google"
 """
-  $('#input-md').val filling
-  html = converter.makeHtml($('#input-md').val())
-  $('#html-md').text html
-  $('#output-md').html html
-    
+  window.resetMd = () ->    
+    $('#input-md').val markdown
+    html = converter.makeHtml($('#input-md').val())
+    $('#html-md').text html
+    $('#output-md').html html
+  
+  resetMd()
+
+  humanMarkup = """
+This is a Heading
+
+Here comes a paragraph.
+
+And another paragraph.
+
+This is a secondary Heading
+Cause the paragraph comes right away.
+
+This should be bold! This should be normal. Bold! Normal? Works.
+
+
+"""
+  window.resetHuman = () ->    
+    $('#input').val humanMarkup
+    hm.run()
+
+  resetHuman()
+  
   $('#input-md').on 'keyup', () ->
     html = converter.makeHtml($('#input-md').val())
     console.log html
--- playground/javascripts/human_markup.js
+++ playground/javascripts/human_markup.js
@@ -3,7 +3,13 @@
 
   HumanMarkup = (function() {
 
-    HumanMarkup.heading_regex = /([\w ,\.-_!?]*)\n\n/g;
+    HumanMarkup.h1_regex = /^(\w.*[^\W])$(\n\n+)/gm;
+
+    HumanMarkup.h2_regex = /^(\w.*[^\W])$(\n)/gm;
+
+    HumanMarkup.p_regex = /^(\w.*[\W])$(\n\n+)/gm;
+
+    HumanMarkup.strong_regex = /([\w ]*[.?])?([\w. ]*[!])/g;
 
     function HumanMarkup(input, html, output) {
       var _this = this;
@@ -20,17 +26,38 @@
       return this.output.html(this.process(this.input.val()));
     };
 
-    HumanMarkup.prototype.process = function(input) {
-      var result;
-      result = input;
-      result = this.detectHeading(result);
-      return result;
+    HumanMarkup.prototype.process = function(text) {
+      text = this.markdownBefore(text);
+      text = this.detectHeadings(text);
+      text = this.detectParagraphs(text);
+      text = this.detectBolds(text);
+      return text = this.markdownAfter(text);
     };
 
-    HumanMarkup.prototype.detectHeading = function(input) {
-      var output;
-      output = input.replace(HumanMarkup.heading_regex, "<h1>$1</h1>\n");
-      return output;
+    HumanMarkup.prototype.detectHeadings = function(text) {
+      text = text.replace(HumanMarkup.h1_regex, "<h1>$1</h1>\n\n");
+      return text = text.replace(HumanMarkup.h2_regex, "<h2>$1</h2>\n\n");
+    };
+
+    HumanMarkup.prototype.detectParagraphs = function(text) {
+      return text = text.replace(HumanMarkup.p_regex, "<p>$1</p>\n\n");
+    };
+
+    HumanMarkup.prototype.detectBolds = function(text) {
+      return text = text.replace(HumanMarkup.strong_regex, "$1<strong>$2</strong>");
+    };
+
+    HumanMarkup.prototype.markdownBefore = function(text) {
+      text = text.replace(/~/g, "~T");
+      text = text.replace(/\$/g, "~D");
+      text = text.replace(/\r\n/g, "\n");
+      text = text.replace(/\r/g, "\n");
+      return text = text.replace(/^[ \t]+$/mg, "");
+    };
+
+    HumanMarkup.prototype.markdownAfter = function(text) {
+      text = text.replace(/~D/g, "$$");
+      return text = text.replace(/~T/g, "~");
     };
 
     return HumanMarkup;
@@ -38,15 +65,26 @@
   })();
 
   $(function() {
-    var converter, filling, html;
-    new HumanMarkup($('#input'), $('#html'), $('#output'));
+    var converter, humanMarkup, markdown;
+    window.hm = new HumanMarkup($('#input'), $('#html'), $('#output'));
     converter = new Markdown.Converter();
-    filling = "# Hi, I'm Markdown\n\nWhat can **I do** for *you*?\n\n## Lists\n### Unordered\n\n- one\n- two\n- three\n\n### Ordered\n\n1. eins\n2. zwei\n3. drei\n\n## Links\nWanna  [google](http://google.com/ \"Looking for something?\") something? Are you speaking german? Try [this][de].\n\n## Quotes\n\n> Email-style angle brackets\nare used for blockquotes.  \nYou don't have to repeat them on every line. \n\n> > And, they can be nested.\n\n> #### Headers in blockquotes\n> \n> * You can quote a list.\n> * Etc.\n\n## Images\n![12sites](http://12sites.de/assets/12sites_large.png \"Visit 12sites\")\n\nPsst: they work reference style like links, too.\n\n## Horizontal rules\n\nThis rule rules\n***\nThis does, too:\n\n---\n\n## Line Breaks\nIf a line  \nends with  \ntwo spaces  \n\n## Code\n`<header>` is a new HTML5 tag.\n\n\n[de]: http://google.de/  \"German Google\"";
-    $('#input-md').val(filling);
-    html = converter.makeHtml($('#input-md').val());
-    $('#html-md').text(html);
-    $('#output-md').html(html);
+    markdown = "# Hi, I'm Markdown\n\nWhat can **I do** for *you*?\n\n## Lists\n### Unordered\n\n- one\n- two\n- three\n\n### Ordered\n\n1. eins\n2. zwei\n3. drei\n\n## Links\nWanna  [google](http://google.com/ \"Looking for something?\") something? Are you speaking german? Try [this][de].\n\n## Quotes\n\n> Email-style angle brackets\nare used for blockquotes.  \nYou don't have to repeat them on every line. \n\n> > And, they can be nested.\n\n> #### Headers in blockquotes\n> \n> * You can quote a list.\n> * Etc.\n\n## Images\n![12sites](http://12sites.de/assets/12sites_large.png \"Visit 12sites\")\n\nPsst: they work reference style like links, too.\n\n## Horizontal rules\n\nThis rule rules\n***\nThis does, too:\n\n---\n\n## Line Breaks\nIf a line  \nends with  \ntwo spaces  \n\n## Code\n`<header>` is a new HTML5 tag.\n\n\n[de]: http://google.de/  \"German Google\"";
+    window.resetMd = function() {
+      var html;
+      $('#input-md').val(markdown);
+      html = converter.makeHtml($('#input-md').val());
+      $('#html-md').text(html);
+      return $('#output-md').html(html);
+    };
+    resetMd();
+    humanMarkup = "This is a Heading\n\nHere comes a paragraph.\n\nAnd another paragraph.\n\nThis is a secondary Heading\nCause the paragraph comes right away.\n\nThis should be bold! This should be normal. Bold! Normal? Works.\n\n";
+    window.resetHuman = function() {
+      $('#input').val(humanMarkup);
+      return hm.run();
+    };
+    resetHuman();
     return $('#input-md').on('keyup', function() {
+      var html;
       html = converter.makeHtml($('#input-md').val());
       console.log(html);
       $('#html-md').text(html);
